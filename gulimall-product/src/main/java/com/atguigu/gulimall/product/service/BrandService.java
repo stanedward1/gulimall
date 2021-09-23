15ca2394ef8e5e18ffb9380c3f8c8bad7293fdc6
longbiu <1679648995@qq.com>
2021-09-24 00:33:05 +0800

    还没有解决的空指针问题

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/BrandService.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/BrandService.java
@@ -3,7 +3,9 @@ package com.atguigu.gulimall.product.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.gulimall.product.entity.BrandEntity;
+import org.springframework.stereotype.Service;
 
+import javax.jws.WebService;
 import java.util.Map;
 
 /**
@@ -13,6 +15,7 @@ import java.util.Map;
  * @email longbiu@foxmail.com
  * @date 2021-09-20 20:40:18
  */
+@Service
 public interface BrandService extends IService<BrandEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
