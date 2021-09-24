dc12692f63e04c9a7a80b08f1173fd2251cb648a
longbiu <1679648995@qq.com>
2021-09-24 23:11:57 +0800

    Fix '空指针错误‘

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -2,6 +2,7 @@ package com.atguigu.gulimall.product;
 
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
+@SpringBootTest
 class GulimallProductApplicationTests {
 
     @Autowired
@@ -22,6 +26,11 @@ class GulimallProductApplicationTests {
         brandEntity.setName("华为");
         brandService.save(brandEntity);
         System.out.println("保存成功！！！");
+
+//        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+//        list.forEach(item)->{
+//            System.out.println(item);
+//        };
     }
 
 }
--- gulimall.common/src/main/java/com/atguigu/common/xss/HTMLFilter.java
+++ gulimall.common/src/main/java/com/atguigu/common/xss/HTMLFilter.java
@@ -1,4 +1,4 @@
-package com.atguigu.common.utils.xss;
+package com.atguigu.common.xss;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
